ba28e8676f78696801a8d4e38f12cab22264c029
Abhinav Jain <abinavjain807@gmail.com>
2025-02-24 13:50:43 -0500

    Beautified Code

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -4,8 +4,8 @@ export async function GET() {
   const books = await fetchBooks();
   return new Response(
     `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url><loc>https://sheetbook.iabhinavj.com/</loc></url>
-      ${books.map(book => `<url><loc>https://sheetbook.iabhinavj.com/book/${book.slug}</loc></url>`).join('')}
+      <url><loc>https://sheet-book.pages.dev/</loc></url>
+      ${books.map(book => `<url><loc>https://sheet-book.pages.dev/book/${book.slug}</loc></url>`).join('')}
     </urlset>`,
     { headers: { "Content-Type": "application/xml" } }
   );
